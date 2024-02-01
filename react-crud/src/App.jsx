3b82bf0812f3f5749f1a9ce1a685bd8d218c47b9
krissyweber <122844703+krissyweber@users.noreply.github.com>
2024-01-31 19:51:36 -0500

    edit/delete product

--- react-crud/src/App.jsx
+++ react-crud/src/App.jsx
@@ -2,21 +2,28 @@ import {Routes, Route, Link} from "react-router-dom"
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
 import EditPage from "./pages/EditPage";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
+export const VITE_BACKEND_URL = import.meta.env.VITE_BACKEND_URL;
 
 const App = () => {
   return (
     <div>
       <nav className="bg-gray-800">
-        <div className="container mx-auto p-2">
+        <div className="container mx-auto p-2 h-full">
           <Link to="/"><h2 className="text-white text-2xl font-bold">React CRUD Web App</h2></Link>
         </div>
       </nav>
-
+<div className="container mx-auto p-2">
       <Routes>
         <Route index element={<HomePage/>}></Route>
         <Route path="/create" element={<CreatePage/>}></Route>
-        <Route path="/edit" element={<EditPage/>}></Route>
+        <Route path="/edit/:id" element={<EditPage/>}></Route>
       </Routes>
+
+</div>
+  <ToastContainer/>   
     </div>
   );
 }
